b6f447601153e356ccaa45977215bf9ded63849d
Panagiotis Tigas <ptigas@gmail.com>
2020-11-22 22:36:49 +0000

    feature complete

--- player.py
+++ player.py
@@ -11,7 +11,7 @@ os.environ['SDL_VIDEODRIVER'] = 'dummy'
 os.environ['DISPLAY'] = ':0'
 import pygame
 pygame.init()
-pygame.display.set_mode((1,1))
+#pygame.display.set_mode((1,1))
 
 freq = 44100    # audio CD quality
 bitsize = -16   # unsigned 16 bit
@@ -20,8 +20,12 @@ buffer = 2048   # number of samples (experiment to get right sound)
 pygame.mixer.init(freq, bitsize, channels, buffer)
 
 # setting up GPIO
+pins = [23, 2, 3, 4, 17, 27, 22]
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(15, GPIO.OUT)
+for pin in pins:
+	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.output(15, GPIO.LOW)
 
 led = False
@@ -35,6 +39,21 @@ def toggle_led(force_led=None):
 		GPIO.output(15, GPIO.LOW)
 	else:
 		GPIO.output(15, GPIO.HIGH)
+
+pushed = False
+def read_buttons():
+	global pushed
+	i = 0
+	for i, pin in enumerate(pins):
+               if not GPIO.input(pin):
+			if not pushed:
+				pushed = True
+				return i
+			else:
+				pushed = True
+				return -1
+	pushed = False
+	return -1
 
 i = 0
 while True:
@@ -52,23 +71,21 @@ while True:
 
 	toggle_led(True)
 
-	for event in pygame.event.get():
-		print(event.type)
-		if event.type == KEYDOWN:
-			key = event.key - 256
-			print(event.key)
-			if key >= 1 and key <= 9:
-				if pygame.mixer.music.get_busy():
-					print("busy")
-					continue
-				print('playing sound {}'.format(key))
-				try:
-					pygame.mixer.music.load('/media/usb0/{}.mp3'.format(key))
-					pygame.mixer.music.play()
-				except:
-					print("no file found")
+	button = read_buttons()
+	if button >= 0:
+		key = button
+		print('pressed key {}'.format(key))
+		if key >= 1 and key <= 9:
+			if pygame.mixer.music.get_busy():
+				print("busy")
+				continue
+			print('playing sound {}'.format(key))
+			try:
+				pygame.mixer.music.load('/media/usb0/{}.mp3'.format(key))
+				pygame.mixer.music.play()
+			except:
+				print("no file found")
 
-			if key == 0:
-				print('stopping')
-				pygame.mixer.music.fadeout(3000)
-				
+		if key == 0:
+			print('stopping')
+			pygame.mixer.music.fadeout(3000)
