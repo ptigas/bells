b87c545415af680fae61241a96029d5105cbedf3
panos <ptigas@gmail.com>
2020-12-08 12:35:54 +0000

    fixed player

--- player.py
+++ player.py
@@ -2,6 +2,7 @@ import os
 import pygame
 from pygame.locals import *
 import RPi.GPIO as GPIO
+import glob
 
 
 mounted = True
@@ -25,67 +26,69 @@ pins = [23, 2, 3, 4, 17, 27, 22]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(15, GPIO.OUT)
 for pin in pins:
-	GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 GPIO.output(15, GPIO.LOW)
 
 led = False
 def toggle_led(force_led=None):
-	global led
-	if force_led is None:
-		led = not led
-	else:
-		led = force_led
-	if not led:
-		GPIO.output(15, GPIO.LOW)
-	else:
-		GPIO.output(15, GPIO.HIGH)
+        global led
+        if force_led is None:
+                led = not led
+        else:
+                led = force_led
+        if not led:
+                GPIO.output(15, GPIO.LOW)
+        else:
+                GPIO.output(15, GPIO.HIGH)
 
 pushed = False
 def read_buttons():
-	global pushed
-	i = 0
-	for i, pin in enumerate(pins):
+        global pushed
+        i = 0
+        for i, pin in enumerate(pins):
                if not GPIO.input(pin):
-			if not pushed:
-				pushed = True
-				return i
-			else:
-				pushed = True
-				return -1
-	pushed = False
-	return -1
+                        if not pushed:
+                                pushed = True
+                                return i
+                        else:
+                                pushed = True
+                                return -1
+        pushed = False
+        return -1
 
 i = 0
 while True:
-	i += 1
-	if not os.path.ismount('/media/usb0'):
-		if mounted:
-			print('not mounted')
-		mounted = False
-		if (i % 10000) == 0: toggle_led()
-		continue
+        i += 1
+        if not os.path.ismount('/media/usb0'):
+                if mounted:
+                        print('not mounted')
+                mounted = False
+                if (i % 10000) == 0: toggle_led()
+                continue
 
-	if not mounted:
-		print('mounted')
-	mounted = True
+        if not mounted:
+                print('mounted')
+        mounted = True
 
-	toggle_led(True)
+        toggle_led(True)
+        files = sorted(glob.glob('/media/usb0/*.mp3'))
+        import pdb; pdb.set_trace()        
+        button = read_buttons()
+        if button >= 0:
+                key = button
+                print('pressed key {}'.format(key))
+                if key >= 1 and key <= 9:
+                        if pygame.mixer.music.get_busy():
+                                print("busy")
+                                continue
+                        print('playing sound {}'.format(key))
+                        try:
+                                pygame.mixer.music.load(files[key-1])
+                                #pygame.mixer.music.load('/media/usb0/{}.mp3'.format(key))
+                                pygame.mixer.music.play()
+                        except:
+                                print("no file found")
 
-	button = read_buttons()
-	if button >= 0:
-		key = button
-		print('pressed key {}'.format(key))
-		if key >= 1 and key <= 9:
-			if pygame.mixer.music.get_busy():
-				print("busy")
-				continue
-			print('playing sound {}'.format(key))
-			try:
-				pygame.mixer.music.load('/media/usb0/{}.mp3'.format(key))
-				pygame.mixer.music.play()
-			except:
-				print("no file found")
-
-		if key == 0:
-			print('stopping')
-			pygame.mixer.music.fadeout(3000)
+                if key == 0:
+                        print('stopping')
+                        pygame.mixer.music.fadeout(3000)
